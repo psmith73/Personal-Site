a6182eb1eeb6cb09f9c34dfd8dfcecc43dadfe61
Philip Smith <philipm.smith@icloud.com>
2019-10-24 14:00:52 -0500

    Hover

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-$(document).ready(function() {
-    $('.hover').on('touchstart touchend', function(e) {
-        e.preventDefault();
-        $(this).toggleClass('hover_effect');
-    });
-});
+$('body').on('touchstart', function() {});
